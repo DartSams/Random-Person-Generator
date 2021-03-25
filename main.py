c483cdf0b870f05028f92bc95948caecb0f5e754
DartSams <dartagnansams1@icloud.com>
2021-03-25 10:40:08 -0400

    fixed OOP to json

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random
 from PIL import ImageTk, Image
 import datetime
 import json
+import jsonpickle
+from json import JSONEncoder
 
 root = Tk()
 root.title("Random Person Generator")
@@ -11,17 +13,59 @@ screen_height = root.winfo_screenheight()
 root.geometry(f"{screen_width}x{screen_height}")
 
 
+class Person:
+    def __init__(
+        self,
+        FirstName,
+        LastName,
+        StreetAddress,
+        State,
+        phone,
+        birthday,
+        age,
+        month,
+        zodiac,
+        email,
+        visa,
+        card_nums,
+        ExperationDate,
+        CVV,
+        majors,
+        height,
+        weight,
+    ):
+        self.FirstName = FirstName
+        self.LastName = LastName
+        self.StreetAddress = StreetAddress
+        self.State = State
+        self.phone = phone
+        self.birthday=birthday
+        self.age = age
+        self.month = month
+        self.zodiac = zodiac
+        self.email = email
+        self.visa = visa
+        self.card_nums=card_nums
+        self.ExperationDate = ExperationDate
+        self.CVV = CVV
+        self.majors = majors
+        self.height = height
+        self.weight = weight
+    
 
-#converting height to centimeters and rounding to the 2nd decimal place
+
+
+
+
 def height_to_centimeters(height):
     height = height.replace("'", ".")
     return str(round(float(height) * 30.48, 2))
 
-#converting pounds to kilograms and rounding the the 2nd decimal place
+
 def pounds_to_kilogram(weight):
     return str(round(float(weight) / 2.205, 2))
 
-#accounting for days in a month
+
 def february_change(month):
     if month == "February":
         return random.randint(1, 28)
@@ -30,7 +74,7 @@ def february_change(month):
     else:
         return random.randint(1, 31)
 
-#clear the screen and then generate another person
+
 def close_all():
     for widget in root.winfo_children():
         widget.grid_forget()
@@ -39,9 +83,9 @@ def close_all():
 
     main()
 
-#creating a json file and then exporting the data to it
+
 def export_json(data):
-    with open("Generated Data.txt", "w") as DataFile:
+    with open("Generated Data.json", "w") as DataFile:
         json.dump(data, DataFile, indent=2)
 
 
@@ -52,7 +96,7 @@ def main():
     img.image = card
     img.grid(row=0, column=0, rowspan=3)
 
-#reading the names in the csv file then splitting on every comma and assigning two of them to first and last names
+    NameLst = []
     with open(r"RPG\name.csv", "r") as NameFile:
         EachName = NameFile.readlines()
         FirstName = random.choice(EachName).split(",")[1].replace("\n", "")
@@ -61,11 +105,12 @@ def main():
     Name_label = Label(root, text=f"{FirstName} {LastName}", font=("Calibri bold", 20))
     Name_label.grid(row=0, column=1)
 
-#reading the address in the address.txt file then splits them on every comma to the the street addres and the state and area number
     with open(r"RPG\address.txt", "r") as AddressFile:
         AddressLst = AddressFile.readlines()
+        # print(AddressLst)
         Street = random.choice(AddressLst).split(",")
         State = Street[1]
+        # print(State)
 
     Address_label = Label(root, text=Street[0], font=("Calibri", 12))
     Address_label.grid(row=1, column=1)
@@ -73,7 +118,7 @@ def main():
     State_label.grid(row=2, column=1)
 
     PhoneNumber = f"{random.randint(100,999)}-{random.randint(100,999)}-{random.randint(1000,9999)}"
-
+    # print(PhoneNumber)
     phone_label = Label(root, text="PHONE:")
     phone_label.grid(row=3, column=1)
 
@@ -81,6 +126,7 @@ def main():
     current_phone_label.grid(row=3, column=2, padx=20)
 
     age_num = random.randint(18, 99)
+    # print(age)
 
     months = {
         "January": "Capricorn",
@@ -97,9 +143,10 @@ def main():
         "December": "Sagittarius",
     }
     month, zodiac = random.choice(list(months.items()))
-
+    # print(month,zodiac)
     birthyear = random.randint(1990, 2021)
     birthday = f"{month} {february_change(month)},{birthyear}"
+    # print(birthday)
 
     birthday_tag = Label(root, text="BIRTHDAY", font=("Calibri bold", 14))
     birthday_tag.grid(row=4, column=1)
@@ -110,7 +157,7 @@ def main():
 
     current_year = datetime.datetime.now()
     current_year = current_year.year
-
+    # print(current_year.year)
     correct_age = current_year - birthyear
 
     current_age_label = Label(root, text="Age:")
@@ -124,7 +171,7 @@ def main():
     current_zodiac.grid(row=7, column=2)
 
     email = f"{FirstName}{LastName}@gmail.com"
-
+    # print(email)
     Social_tag = Label(root, text="SOCIALS", font=("Calibri bold", 14))
     Social_tag.grid(row=8, column=1)
     current_email_label = Label(root, text="Email:")
@@ -136,8 +183,11 @@ def main():
     card = random.choice(card_lst)
 
     card_nums = f"{random.randint(1000,9999)} {random.randint(1000,9999)} {random.randint(1000,9999)} {random.randint(1000,9999)}"
+    # print(Visa)
     ExperirationDate = f"{random.randint(1,12)}/{random.randint(2021,2030)}"
+    # print(ExperirationDate)
     CVV = random.randint(100, 999)
+    # print(CVV)
 
     finance_tag = Label(root, text="FINANCE", font=("Calibri bold", 14))
     finance_tag.grid(row=10, column=1)
@@ -198,7 +248,7 @@ def main():
     ]
 
     major = random.choice(majors)
-
+    # print(random.choice(majors))
     major_tag = Label(root, text="EMPLOYMENT", font=("Calibri bold", 14))
     major_tag.grid(row=14, column=1)
     occupation_label = Label(root, text="Occupation:")
@@ -207,9 +257,13 @@ def main():
     occupation.grid(row=15, column=2)
 
     height = f"{random.randint(4,6)}'{random.randint(0,10)}"
+    # print(height)
+    # print(height_to_centimeters(height))
     centimeter = height_to_centimeters(height)
 
     weight = random.randint(100, 300)
+    # print(weight)
+    # print(pounds_to_kilogram(weight))
     kilogram = pounds_to_kilogram(weight)
 
     physical_traits_tag = Label(root, text="PHYSICAL TRAITS", font=("Calibri bold", 14))
@@ -223,34 +277,40 @@ def main():
     current_weight = Label(root, text=(f"{weight} pounds ({kilogram} kilogram)"))
     current_weight.grid(row=18, column=2)
 
+    generate=Person(FirstName,LastName,Street[0],State,PhoneNumber,birthday,correct_age,month,zodiac,email,card,card_nums,ExperirationDate,CVV,major,height,weight)
+    print(generate.age)
 
-#collect data in JSON format
+    
+
+
     data = {
-        "Name": {"first": FirstName, "last": LastName},
-        "address": Street,
-        "phone": PhoneNumber,
-        "date of birth": birthday,
-        "age": correct_age,
-        "zodiac": zodiac,
-        "email": email,
+        "Name": {"first": generate.FirstName, "last": generate.LastName},
+        "address": generate.StreetAddress,
+        "phone": generate.phone,
+        "date of birth": generate.birthday,
+        "age": generate.age,
+        "zodiac": generate.zodiac,
+        "email": generate.email,
         "finance": {
-            "card": card,
-            "16 digits": card_nums,
-            "experiation date": ExperirationDate,
-            "cvv": CVV,
+            "card": generate.visa,
+            "16 digits": generate.card_nums,
+            "experiation date": generate.ExperationDate,
+            "cvv": generate.CVV,
         },
-        "occupation": major,
-        "physical traits": {"height": height, "weight": weight},
+        "occupation": generate.majors,
+        "physical traits": {"height": generate.height, "weight": generate.weight},
     }
 
 
-    generate = Button(root, text="Generate", command=lambda: close_all())
-    generate.grid(row=3, column=0)
+    generate_another_person = Button(root, text="Generate", command=lambda: close_all())
+    generate_another_person.grid(row=3, column=0)
 
     export_to_json = Button(root, text="Export to JSON", command=lambda: export_json(data))
     export_to_json.grid(row=4, column=0)
 
     root.mainloop()
+
+    
 
 
 if __name__ == "__main__":
